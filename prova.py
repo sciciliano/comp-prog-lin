008cb028b2271357898439aff98d2d911f9005da
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-03-13 16:36:53 +0100

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,3 +1,28 @@
 import streamlit as st
-title = st.text_input('Please insert a movie title', '')
-st.write('The current movie title is', title)
+import json, requests 
+
+# add your own APIkey
+APIkey = 'b0dc5ff479faf43dff849169f51ad2b0'
+location = 'Bolzano'
+
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+# print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+# print(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+st.text("current max temperature in Bolzano",str(weatherData['main']['temp_max'])) 
+# more???????????
