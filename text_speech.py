4b64cf50dadb2290aa660e763a835d99c57fe157
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-04-11 16:11:40 +0200

    Update text_speech.py

--- text_speech.py
+++ text_speech.py
@@ -2,7 +2,6 @@ from gtts import gTTS
 import json, requests
 import streamlit as st
 from googletrans import Translator
-import IPython.display as ipd  
 
 translator = Translator()
 input_str = st.text_input('type here some text!\t')
@@ -10,6 +9,7 @@ input_lang = st.selectbox('type the language',('en','de','it','es'))
 trans_str= translator.translate(input_str, dest= input_lang)
 tts1=gTTS(text= trans_str.text, lang= input_lang)
 tts1.save('helloworld.mp3')
+
 if (trans_str and input_lang):
     audio_file = open('helloworld.mp3','rb')
     st.audio(data=audio_file, format="audio/mp3", start_time=0)
