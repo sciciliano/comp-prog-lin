142eb354835cb784f46e1a3aed253e619acb4f4c
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-04-11 15:29:27 +0200

    Update trans_word.py

--- trans_word.py
+++ trans_word.py
@@ -6,6 +6,6 @@ translator = Translator()
 word = st.text_input('ask me a word: ','')
 target_language = st.selectbox('Please select a language: ',('it','en','de','es'))
 
-if (language and keyword):
+if (language and target_language):
   target_word = translator.translate(word ,dest=target_language)
   st.write(target_word)
