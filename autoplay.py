d14676248738034283b581688e851f53d94f86ad
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-04-11 16:45:12 +0200

    Update autoplay.py

--- autoplay.py
+++ autoplay.py
@@ -4,13 +4,6 @@ import base64
 import streamlit as st
 from googletrans import Translator
 
-translator = Translator()
-input_str = st.text_input('type here some text!\t')
-input_lang = st.selectbox('type the language',('en','de','it','es'))
-trans_str= translator.translate(input_str, dest= input_lang)
-tts1=gTTS(text= trans_str.text, lang= input_lang)
-tts1.save('helloworld.mp3')
-
 def autoplay_audio(file_path: str):
     with open(file_path, "rb") as f:
         data = f.read()
@@ -24,6 +17,14 @@ def autoplay_audio(file_path: str):
             md,
             unsafe_allow_html=True,
         )
+
+translator = Translator()
+input_str = st.text_input('type here some text!\t')
+input_lang = st.selectbox('type the language',('en','de','it','es'))
+trans_str= translator.translate(input_str, dest= input_lang)
+tts1=gTTS(text= trans_str.text, lang= input_lang)
+tts1.save('helloworld.mp3')
+
 
 if (trans_str and input_lang):
     autoplay_audio(audio_file)
