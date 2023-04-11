af2bc505379eff759b9f2590f5eeaecab7121a15
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-04-11 14:57:06 +0200

    Update trans_word.py

--- trans_word.py
+++ trans_word.py
@@ -4,5 +4,5 @@ import streamlit as st
 
 translator = Translator()
 word = st.text_input('ask me a word: ','')
-target_language = st.selectbox('it','en','de','es',)
+target_language = st.selectbox('Please select a language: ',('it','en','de','es'))
 target_word = st.write(translator.translate(word ,dest=target_language))
