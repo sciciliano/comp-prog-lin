c294010aad5dabc451c7c2a6806358cbcecf71b9
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-04-11 16:36:02 +0200

    Create autoplay.py

---
+++ autoplay.py
@@ -0,0 +1,23 @@
+import base64
+ 
+import streamlit as st
+ 
+ 
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+ 
+ 
+st.write("# Auto-playing Audio!")
+ 
+autoplay_audio("local_audio.mp3")
