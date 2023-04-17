33d1a4351c6d635217b9552b8df88a2724dee46b
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-04-17 13:38:44 +0200

    Create translate_download.py

---
+++ translate_download.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import json, requests
+from googletrans import Translator
+
+translator = Translator()
+input_txt = st.file_uploader("Upload file", type=['txt'])
+input_str = uploaded_file.getvalue().decode("utf-8")
+input_lang = st.selectbox('type the language',('en','de','it','es'))
+trans_str= translator.translate(input_str, dest= input_lang)
+
+
+if (trans_str and input_lang):
+    st.write(trans_str)
+    st.download_button(label="Download your translated file", data=text_file, file_name='yourfile.mp3',mime='text/txt')
