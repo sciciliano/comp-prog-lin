5e2de4508a42632adc48980c40310a69473c55d7
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-04-17 14:15:50 +0200

    Update translate_download.py

--- translate_download.py
+++ translate_download.py
@@ -4,11 +4,9 @@ from googletrans import Translator
 
 translator = Translator()
 input_txt = st.file_uploader("Upload file", type=['txt'])
-input_str = input_txt.getvalue().decode("utf-16")
+input_str = input_txt.getvalue().decode("utf-8")
 input_lang = st.selectbox('type the language',('en','de','it','es'))
 trans_str= translator.translate(input_str, dest= input_lang)
 
-
-if (trans_str and input_lang):
-    st.write(trans_str)
+st.write(trans_str)
 st.download_button(label="Download your translated file", data=trans_str, file_name='transfile.txt',mime='text/txt')
