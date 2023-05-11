bd71300cf3c18ea0bf2f0aee9a6092f766e0126c
sciciliano <127765808+sciciliano@users.noreply.github.com>
2023-05-11 09:32:57 +0200

    Update stt.py

--- stt.py
+++ stt.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import speech_recognition as sr
+from audio_recorder_streamlit import audio_recorder
 
+audio_bytes = audio_recorder()
 r = sr.Recognizer()
-file = st.file_uploader("Upload file", type=['wav'])
-with sr.AudioFile(file) as source:
+my_audio=st.audio(audio_bytes, format="audio/wav")
+if audio_bytes:
+    my_audio
+    my_audio.save('my_audio.mp3')
+with my_audio as source:
     audio= r.record(source)
 
 recognized_text = r.recognize_google(audio)
